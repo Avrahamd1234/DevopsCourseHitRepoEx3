d57c226a6cc56828f37c91b3f0bc844b94418cf8
Avrahamd1234 <avraham91@gmail.com>
2025-08-17 21:01:20 +0300

    modified hello java to show hello universe too to trigger successful jenkins jobs (need 5)

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Updated Print (Not hello world) ..."+i);
+			System.out.println("Updated Print2 (Not hello world, hello universe) ..."+i);
 			//this is a useless comment
 		}
 	}
